dfba31abd42bba73fd328d0fa83d2103ec5088f1
santosh226206 <santoshamcl1996@gmail.com>
2025-02-08 08:08:27 +0530

    first commit

--- src/main/java/santos/runnable/ThreadTestOne.java
+++ src/main/java/santos/runnable/ThreadTestOne.java
@@ -1,4 +1,18 @@
 package santos.runnable;
 
 public class ThreadTestOne {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t=new Thread(()->{
+
+            try {
+                Thread.sleep(10000);
+                System.out.println("hello from rhread 1");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        t.start();
+        t.join();
+        System.out.println("programs-end");
+    }
 }
