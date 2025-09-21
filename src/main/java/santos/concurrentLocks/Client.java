daf036d8e8eebb378db019fe6b6d08b219f0b0c7
santosh226206 <santoshamcl1996@gmail.com>
2025-09-21 11:38:03 +0530

    first commit

--- src/main/java/santos/concurrentLocks/Client.java
+++ src/main/java/santos/concurrentLocks/Client.java
@@ -4,8 +4,8 @@ public class Client {
     public static void main(String[] args) {
         SharedResource sharedResource = new SharedResource();
 
-        Thread producer = new Producer(sharedResource);
-        Thread consumer = new Consumer(sharedResource);
+        Producer producer = new Producer(sharedResource);
+        Consumer consumer = new Consumer(sharedResource);
 
         producer.start();
         consumer.start();
